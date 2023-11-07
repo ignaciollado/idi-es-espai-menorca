f714caa85125a266fd18bda199998ca361f4f8f0
Ignacio Lladó Vidal <ignacio.llado@idi.es>
2023-11-07 07:27:25 +0100

    actualizado get y create .php

--- src/assets/phpAPI/bookingCreate.php
+++ src/assets/phpAPI/bookingCreate.php
@@ -5,19 +5,10 @@ header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 
 require_once 'conectar_a_bbdd.php';
-/* require_once 'encrDecr.php'; */
 
 mysqli_query($conn, "SET NAMES 'utf8'");
 $postedData = file_get_contents("php://input");
 $request = json_decode($postedData, TRUE);
-
-/* var_dump( $request );
-echo $request['bookerEMail']."<br>";
-echo $request['bookerName']."<br>";
-echo $request['fromDate']."<br>";
-echo $request['toDate']."<br>";b
-echo $request['idCard']."<br>";
-echo $request['resourceToBook']."<br>";  */
 
 $sql = "INSERT INTO `booking_service` (name, idCard, email, resourceBooked, fromDate, toDate, allDay) VALUES('"
           .$request['bookerName']."','"
